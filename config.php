88ef8d9a9a52432f3f6743e5ee3262ca7c41ccb3
Leo Gopal <leo@leogopal.com>
2019-01-26 15:07:39 +0200

    Added netlify

--- config.php
+++ config.php
@@ -3,14 +3,14 @@
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'siteName' => 'Coding with Leo Gopal',
+    'siteDescription' => 'Coding tips, tricks, workflows and snippets by Leo Gopal',
+    'siteAuthor' => 'Leo Gopal',
 
     // collections
     'collections' => [
         'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
+            'author' => 'Leo Gopal', // Default author, if not provided in a post
             'sort' => '-date',
             'path' => 'blog/{filename}',
         ],
